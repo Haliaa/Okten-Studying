99090922698167465de2f6babf5d5edb9dbec3e4
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-02-01 22:42:29 +0200

    lesson1,classwork

---
+++ Part2_JAVASCRIPT/lesson1/practic/2.js
@@ -0,0 +1,37 @@
+// tenNumbers = [2,3,2,3,2,3,2,3,2,3]
+// result = tenNumbers[0]+tenNumbers[1]+tenNumbers[2]+tenNumbers[3]+tenNumbers[4]+tenNumbers[5]+tenNumbers[6]+tenNumbers[7]+tenNumbers[8]+tenNumbers[9]
+// let book = {
+//     name:'Aladin',
+//     pages:400,
+//     genre:fairytale,
+// }
+// let book = {
+//     name:'Zhasmin',
+//     pages:400,
+//     genre:fairytale,
+//     authors:['Gena Bukin', 'Pachtalion Pechkin']
+// }
+// let book = [
+//     {name:'Karlson', pages: 400, authors:'Shevchenko'},
+//     {name:'Voroniny', pages: 300, authors:'Gena Bukin'},
+//     {name:'Djek Vorobey', pages: 200, authors:'Pachtalion Pechkin'}
+// ]
+// console.log(book[0].name)
+// console.log(book[1].name)
+// console.log(book[2].name)
+
+
+// 5
+// let height = 23
+// let width = 10
+// let s = height*width
+//
+// // 6
+// let heightC = 10
+// let dC = 4
+// let v = heightC*dC
+
+// 7
+// let n = 3
+// let m = 4
+// let k =Math.sqrt(3**2+4**2)
