53b9316320fd14306dedc2f1b18a2bfa12b9f972
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-02-01 21:38:35 +0200

    lesson1,homework

---
+++ Part2_JAVASCRIPT/lesson1/lection/main.js
@@ -0,0 +1,51 @@
+// console.log('hi')
+// console.log("hey")
+// console.log(`hello`)
+// let number = 100500
+// console.log(number+3)
+// {
+//     let a = 2// локальна змінна
+//     var b = 3// глобальна змінна
+// }
+// console.log(b)// викличеться
+// console.log(a)// не викличеться
+// console.log(typeof true)
+// '5' == 5 //перетворює обох в стрінг і порівнює по табл ASCI
+
+// ARRAY:
+
+// let num = 23
+// let ber = 777;
+// let a = [ber, 12, num, 'andriy', typeof 888, [1,3.8,9]]
+// console.log(a)
+// console.log(a[2])
+// console.log(a.length)
+
+// Object
+
+// let product = {
+//     title:'milk',
+//     quantity:2,
+//     manufacturer:{
+//         country:'Ukraine',
+//         bar_code: 482,
+//         manufacture:'meat&cheese',
+//         conservants: [{name:'a44', status:'dangerous'}, {name:'b8', status:'safe'}, {name:'c03', status:'not recommended'}]
+//     }
+// }
+// console.log(product.title)
+// console.log(product['title'])
+// console.log(product.manufacturer.bar_code)
+// console.log(product['manufacturer'].bar_code)
+// console.log(product['manufacturer']['bar_code'])
+// console.log(product.manufacturer.bar_code)
+// console.log(product.manufacturer.conservants[2].status)
+// // console.log(10%3)//1
+// console.log(!!0)
+// console.log(!!' ')
+// console.log(!!NaN)
+// console.log(!!null)
+// console.log(!!undefined)
+// let msg = 'hey guys'
+// document.write('<div>'+msg+'</div>')
+// document.write(`<div>${msg}</div>`)
---
+++ Part2_JAVASCRIPT/lesson1/practic/1.js
@@ -0,0 +1,132 @@
+// let a1 = 'hello'
+// let a2 = 'owu'
+// let a3 = 'ua'
+// let a4 = 1
+// let a5 = 10
+// let a6 = -999
+// let a7 = 123
+// let a8 = 3.14
+// let a9 = 2.7
+// let a10 = 16
+// let a11 = true
+// let a12 = false
+//
+// console.log(a1)
+// console.log(a2)
+// console.log(a3)
+// console.log(a4)
+// console.log(a5)
+// console.log(a6)
+// console.log(a7)
+// console.log(a8)
+// console.log(a9)
+// console.log(a10)
+// console.log(a11)
+// console.log(a12)
+//
+// alert(a1)
+// alert(a2)
+// alert(a3)
+// alert(a4)
+// alert(a5)
+// alert(a6)
+// alert(a7)
+// alert(a8)
+// alert(a9)
+// alert(a10)
+// alert(a11)
+// alert(a12)
+//
+// document.write('<h1>'+a1+'</h1>')
+// document.write('<h1>'+a2+'</h1>')
+// document.write('<h1>'+a3+'</h1>')
+// document.write('<h1>'+a4+'</h1>')
+// document.write('<h1>'+a5+'</h1>')
+// document.write('<h1>'+a6+'</h1>')
+// document.write('<h1>'+a7+'</h1>')
+// document.write('<h1>'+a8+'</h1>')
+// document.write('<h1>'+a9+'</h1>')
+// document.write('<h1>'+a10+'</h1>')
+// document.write('<h1>'+a11+'</h1>')
+// document.write('<h1>'+a12+'</h1>')
+//
+// a1 = 'Hello'
+// a2 = 'hello'
+// a3 = 'один'
+// a4 = 'момент'
+// a5 = 'Твоей'
+// a6 = 'руке'
+// a7 = 'так'
+// a8 = 'не'
+// a9 = 'уютно'
+// a10 = 'в'
+// a11 = 'моей'
+// a12 = 'руке'
+//
+// console.log(a1)
+// console.log(a2)
+// console.log(a3)
+// console.log(a4)
+// console.log(a5)
+// console.log(a6)
+// console.log(a7)
+// console.log(a8)
+// console.log(a9)
+// console.log(a10)
+// console.log(a11)
+// console.log(a12)
+//
+// alert(a1)
+// alert(a2)
+// alert(a3)
+// alert(a4)
+// alert(a5)
+// alert(a6)
+// alert(a7)
+// alert(a8)
+// alert(a9)
+// alert(a10)
+// alert(a11)
+// alert(a12)
+//
+// document.write('<h1>'+a1+'</h1>')
+// document.write('<h1>'+a2+'</h1>')
+// document.write('<h1>'+a3+'</h1>')
+// document.write('<h1>'+a4+'</h1>')
+// document.write('<h1>'+a5+'</h1>')
+// document.write('<h1>'+a6+'</h1>')
+// document.write('<h1>'+a7+'</h1>')
+// document.write('<h1>'+a8+'</h1>')
+// document.write('<h1>'+a9+'</h1>')
+// document.write('<h1>'+a10+'</h1>')
+// document.write('<h1>'+a11+'</h1>')
+// document.write('<h1>'+a12+'</h1>')
+
+// let firstName = 'Wolkgan'
+// let middleName = 'Emily'
+// let lastName = 'Andriyvna'
+// let person = firstName+' '+middleName+' '+lastName
+// console.log(person)
+
+// let a=prompt('Please, enter your name:')
+// let b=prompt('surname:')
+// let c=prompt('age:')
+// console.log('Вітаю '+a+' '+b+'. Тобі '+c+' років!')
+// let a = 100; let b = '100'; let c = true;
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(5 != 6)
+// console.log(5 == 6)
+// console.log(5 > 6)
+// console.log(5 === 6)
+
+
+// console.log(10 == 10)
+// console.log(10 === 10)
+// console.log(10 != 10)
+// console.log(10 > 10)
+// console.log(10 < 10)
+
+// console.log(123 > '123')
+// console.log(123 >= '123')
