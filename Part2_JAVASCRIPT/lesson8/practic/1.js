32ab454dd79d83031f78ffc5de03b16eb37b7ed8
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-02-28 13:26:24 +0200

    lesson8, homework

---
+++ Part2_JAVASCRIPT/lesson8/practic/1.js
@@ -0,0 +1,40 @@
+// //     - Напишіть код,  котрий :
+// // -- отримує текст з параграфа з id "content"
+// let textOfContentId = document.getElementById('content').innerHTML;
+// console.log(textOfContentId);
+
+// // -- отримує текст з блоку з id "rules"
+// let textOfRulesId = document.getElementById('rules').innerHTML;
+// console.log(textOfRulesId);
+
+// // -- замініть текст параграфа з id 'content' на будь-який інший
+// let textOfContentId = document.getElementById('content').innerHTML;
+// textOfContentId ='Змінений текст id content'
+// console.log(textOfContentId);
+
+// // -- замініть текст параграфа з id 'rules' на будь-який інший
+//  let textOfRulesId = document.getElementById('rules').innerHTML;
+//  textOfRulesId = 'Змінений текст id rules'
+//  console.log(textOfRulesId)
+
+// // -- змініть кожному елементу колір фону на червоний
+// let pageElements = document.body.children;
+// console.log(pageElements);
+// for (const pageElement of pageElements) {
+//     pageElement.style.background = 'red'
+// }
+
+// // -- змініть кожному елементу колір тексту на синій
+// let pageElements = document.body.children;
+// console.log(pageElements);
+// for (const pageElement of pageElements) {
+//     pageElement.style.color = 'red'
+// }
+
+// // -- отримати весь список класів елемента fc_rules і вивести їх в console.log
+
+
+// // -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let fc_rulesList = document.getElementsByClassName('fc_rules');
+// for (const fcRulesListElement of fc_rulesList) {
+//     fcRulesListElement.style.color = 'red'}
