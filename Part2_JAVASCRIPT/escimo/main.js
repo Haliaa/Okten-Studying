ad9a3b9ad9bf8d2450790d7835b19cdf4a069bee
Haliaa <91005548+Haliaa@users.noreply.github.com>
2023-02-01 17:04:32 +0200

    add esimo folder
    add esimo folder

---
+++ Part2_JAVASCRIPT/escimo/main.js
@@ -0,0 +1,93 @@
+    // DOM
+    // console.log(document);
+    // console.log(document.head);
+
+    // // bytag,
+    // let metaList = document.head.getElementsByTagName('meta');
+    // // console.log(metaList);
+    // for (const metaListElement of metaList) {
+    //     console.log('-------start---------')
+    //     // console.log(metaListElement);
+    //     let attributes = metaListElement.attributes;
+    //     // console.log(attributes);
+    //     for (const attribute of attributes) {
+    //         console.log(attribute, attribute.value);
+    //     }
+    //     console.log('-------end---------')
+    // }
+
+    // let titles = document.head.getElementsByTagName('title');
+    // console.log(titles);
+
+    // console.log(document.head.children);
+
+
+    //     byclass,
+    // classlist
+
+    // search in current element
+    // children,
+    // console.log(document.body.getElementsByClassName('product'));
+    // let products = document.getElementsByClassName('product');
+    // // console.log(products);
+    //
+    //
+    // for (const productDiv of products) {
+    //     // console.log(productDiv);
+    //     // productDiv.classList.add('x');
+    //     // productDiv.classList.add('y');
+    //     // productDiv.classList.remove('x');
+    //     // console.log(productDiv.classList.contains('y'));
+    //
+    //     productDiv.style.background = 'gray';
+    //
+    //
+    // }
+    //
+    // let h1OfBlock1 = products[0].children[0];
+    // console.log(h1OfBlock1);
+    // // h1OfBlock1.innerText = '<i>hello</i>';
+    // h1OfBlock1.innerHTML = '<i>hello</i>';
+
+
+    // node / element
+    // console.log(document.body.children); // html elements
+    // console.log(document.body.childNodes); // node - hE text comment
+
+    //     id,
+
+    // let byId = document.getElementById('xxx'); // html element 1<---
+    // console.log(byId);
+
+
+    // querySelector
+
+    // let queryList = document.querySelector('body > .wrap > .product');
+
+    // let queryList = document.querySelectorAll('body > .wrap > .product'); // NODE
+    // console.log(queryList);
+
+    // let xxxList = document.querySelectorAll('#xxx');
+    // console.log(xxxList);
+
+    // formsAPI
+    // creating new element
+    // var element = document.createElement("P");
+    // element.innerText = doc.data().email;
+
+    // adding class to the element
+    // element.classList.add("box");
+
+    // var parentElement = document.getElementById("parent");
+
+    // parentElement.appendChild(element);
+
+    var parentElement = document.getElementById("parent");
+    // creating new element
+    var element = document.createElement("P");
+    // element.innerText = doc.data().email;
+
+    // adding class to the element
+    element.classList.add("box");
+
+    parentElement.appendChild(element);
